0e38ede0e5e0f7d830c2384177fca5f1ab52e403
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-11-10 19:22:58 -0500

    First commit(Addition class cread)

---
+++ Assignment4/src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition implements Runnable {
+	private int firstNum;
+	private int secondNum;
+
+	public Addition(int firstNum, int secondNum) {
+		this.firstNum = firstNum;
+		this.secondNum = secondNum;
+	}
+
+	@Override
+	public void run() {
+
+	}
+}
