6588eeba9251f6f9f1aa6899ab8a8a64a30da913
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-11-10 19:51:17 -0500

    modified Division class

--- Assignment4/src/Division.java
+++ Assignment4/src/Division.java
@@ -3,17 +3,18 @@ public class Division implements Runnable {
 
 	private int firstNum;
 	private int secondNum;
-	
+
 	public Division(int firstNum, int secondNum) {
-		this.firstNum=firstNum;
-		this.secondNum=secondNum;
-		
+		this.firstNum = firstNum;
+		this.secondNum = secondNum;
+
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("The result of the division operation on " + firstNum + "and " + secondNum + " is"
+				+ (firstNum / secondNum));
+
 	}
-	
+
 }
