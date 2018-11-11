033eeb4b65848f6ca26be6ea7a62fdd28a3a2bf6
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-11-10 19:44:23 -0500

    added multipication class

--- Assignment4/src/Mutlipication.java
+++ Assignment4/src/Mutlipication.java
@@ -1,18 +1,18 @@
 
-public class Mutlipication implements Runnable{
+public class Mutlipication implements Runnable {
 	private int firstNum;
 	private int secondNum;
-	
- public Mutlipication (int firstNum, int secondNum) {
-	 this.firstNum=firstNum;
-	 this.secondNum=secondNum;
- }
 
-@Override
-public void run() {
-	System.out.println("The result of the multipication operation on " + firstNum + "and " + secondNum + " is"
-			+ (firstNum *secondNum));
-	
-}
- 
+	public Mutlipication(int firstNum, int secondNum) {
+		this.firstNum = firstNum;
+		this.secondNum = secondNum;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("The result of the multipication operation on " + firstNum + "and " + secondNum + " is"
+				+ (firstNum * secondNum));
+
+	}
+
 }
