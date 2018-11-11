5cb475a190cce846f6dfdfd7f4866a5c497580b6
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-11-10 19:45:14 -0500

    added Division class

---
+++ Assignment4/src/Division.java
@@ -0,0 +1,19 @@
+
+public class Division implements Runnable {
+
+	private int firstNum;
+	private int secondNum;
+	
+	public Division(int firstNum, int secondNum) {
+		this.firstNum=firstNum;
+		this.secondNum=secondNum;
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
