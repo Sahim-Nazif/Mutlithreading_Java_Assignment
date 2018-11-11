a45b3f5ec117af5bd0edd87c34e114e55e706019
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-11-10 19:31:26 -0500

    Added Subtraction class

---
+++ Assignment4/src/Subtract.java
@@ -0,0 +1,17 @@
+
+public class Subtract implements Runnable{
+ private int firstNum;
+ private int secondNum;
+ 
+ public Subtract (int firstNum, int secondNum) {
+	 this.firstNum=firstNum;
+	 this.secondNum=secondNum;
+ }
+
+@Override
+public void run() {
+	
+	
+}
+ 
+}
